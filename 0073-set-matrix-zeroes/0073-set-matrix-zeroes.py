8e9afa971a766f4eba8947754372e4254655165a
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-03-23 19:46:15 +1000

    Time: 0 ms (100%), Space: 18.5 MB (55.68%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -0,0 +1,17 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        m = len(matrix[0])
+        q = deque()
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == 0:
+                    q.append((i, j))
+        while q:
+            x, y = q.popleft()
+            for i in range(n):
+                matrix[i][y] = 0
+            for i in range(m):
+                matrix[x][i] = 0
+    
+        
