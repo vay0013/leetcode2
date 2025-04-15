55e23d4b7d51ad46e5ad87e859d48c145b6a753b
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-04-15 18:54:41 +1000

    Time: 4729 ms (35.96%), Space: 17.8 MB (64.66%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,9 +1,11 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def bt(word, row, col):
+        n = len(board)
+        m = len(board[0])
+        def dfs(word, row, col):
             if len(word) == 0:
                 return True
-            if row<0 or row>=len(board) or col<0 or col>=len(board[0]) or board[row][col]!=word[0]:
+            if row<0 or row>=n or col<0 or col>=m or board[row][col] != word[0]:
                 return False
             match = False
             original = board[row][col]
@@ -11,11 +13,12 @@ class Solution:
             for dx, dy in (0,1), (1,0), (-1,0), (0,-1):
                 x = row + dx
                 y = col + dy
-                match = match or bt(word[1:], x, y)
+                match = match or dfs(word[1:], x, y)
             board[row][col] = original
             return match
-        for i in range(len(board)):
-            for j in range(len(board[i])):
-                if board[i][j] == word[0] and bt(word, i, j):
+
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == word[0] and dfs(word, i, j):
                     return True
         return False
