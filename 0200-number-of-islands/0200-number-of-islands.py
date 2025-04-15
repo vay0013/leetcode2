4c674514ddef676c911e17e61e08fed1c4791f53
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-04-15 19:59:10 +1000

    Time: 292 ms (18.38%), Space: 32.4 MB (5.85%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -6,9 +6,7 @@ class Solution:
         m = len(grid[0])
         result = 0
         seen = set()
-        def is_valid(x, y):
-            return 0<=x<n and 0<=y<m and grid[x][y] == "1"
-        q = deque([])
+        q = deque()
         for i in range(n):
             for j in range(m):
                 if (i, j) in seen:
@@ -18,12 +16,11 @@ class Solution:
                     q.append((i, j))
                     while q:
                         x, y = q.popleft()
-                        if is_valid(x, y):
+                        if 0<=x<n and 0<=y<m and grid[x][y] == "1":
                             grid[x][y] = "0"
-                            for dx, dy in [(0,1),(1,0),(0,-1),(-1,0)]:
-                                new_x = dx+x
-                                new_y = dy+y
+                            for dx, dy in (1,0), (0,1), (-1,0), (0,-1):
+                                new_x = x+dx
+                                new_y = y+dy
                                 q.append((new_x, new_y))
                                 seen.add((new_x, new_y))
-
-        return result
+        return result                       
