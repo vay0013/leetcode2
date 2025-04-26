c6273501f9bfb34b1b68910e2d2caea87aae8ade
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-04-26 19:59:27 +1000

    Time: 255 ms (47.74%), Space: 25 MB (33.94%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,23 +4,19 @@ class Solution:
             return 0
         n = len(grid)
         m = len(grid[0])
-        result = 0
         seen = set()
+        res = 0
         q = deque()
         for i in range(n):
             for j in range(m):
-                if (i, j) in seen:
-                    continue
-                if grid[i][j] == "1":
-                    result += 1
+                if grid[i][j] == "1" and (i, j) not in seen:
+                    res += 1
                     q.append((i, j))
                     while q:
-                        x, y = q.popleft()
-                        if 0<=x<n and 0<=y<m and grid[x][y] == "1":
-                            grid[x][y] = "0"
-                            for dx, dy in (1,0), (0,1), (-1,0), (0,-1):
-                                new_x = x+dx
-                                new_y = y+dy
-                                q.append((new_x, new_y))
-                                seen.add((new_x, new_y))
-        return result                       
+                        row, col = q.popleft()
+                        for dx, dy in (1, 0), (0, 1), (-1, 0), (0, -1):
+                            x, y = row + dx, col + dy
+                            if (0<=x<n and 0<=y<m and (x, y) not in seen and grid[x][y] == "1"):
+                                q.append((x, y))
+                                seen.add((x, y))
+        return res
