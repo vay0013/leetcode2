f6f6e366911d86af675357fe7639066460c89166
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-04-26 23:04:32 +1000

    Time: 26 ms (48.71%), Space: 19.5 MB (17.84%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,23 +1,20 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+
+        
         n = len(grid)
         m = len(grid[0])
         res = 0
         seen = set()
-        q = deque()
+        def bfs(row, col):
+            if not (0<=row<n and 0<=col<m and grid[row][col] and (row, col) not in seen):
+                return 0
+            seen.add((row, col))
+            return 1 + bfs(row+1, col) + bfs(row, col+1) + bfs(row-1, col) + bfs(row, col-1)
+
         for i in range(n):
             for j in range(m):
-                if (grid[i][j] == 1 and (i, j) not in seen):
-                    count = 1
-                    q.append((i, j))
-                    seen.add((i, j))
-                    while q:
-                        row, col = q.popleft()
-                        for dx, dy in (1, 0), (0, 1), (-1, 0), (0, -1):
-                            x, y = row + dx, col + dy
-                            if 0<=x<n and 0<=y<m and (x, y) not in seen and grid[x][y] == 1:
-                                seen.add((x, y))
-                                q.append((x, y))
-                                count += 1
-                    res = max(res, count)
+                if (grid[i][j] == 1):
+                    res = max(bfs(i, j), res)
+        
         return res
