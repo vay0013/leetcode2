87b975621f7b9cf600de0727e28e0e63627c0e6a
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-05-02 16:34:01 +1000

    Time: 2 ms (98.48%), Space: 41.5 MB (96.76%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        Set<Integer> set = new HashSet<>();
+        for(Map.Entry<Integer, Integer> m : map.entrySet()) {
+            if (set.contains(m.getValue())) {
+                return false;
+            }
+            set.add(m.getValue());
+        }
+        return true;
+    }
+}
