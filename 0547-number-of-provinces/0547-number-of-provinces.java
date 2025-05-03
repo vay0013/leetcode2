aab20b76b24c7d78fcee6ddef15b0cafbda86444
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-05-03 17:12:31 +1000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,32 +1,23 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < isConnected.length; i++) {
+            for (int j = i; j < isConnected[i].length; j++) {
                 if (isConnected[i][j] == 1) {
-                    graph.computeIfAbsent(i, k -> new ArrayList<>()).add(j);
-                    graph.computeIfAbsent(j, k -> new ArrayList<>()).add(i);
+                    graph.computeIfAbsent(i, k -> new HashSet<>()).add(j);
+                    graph.computeIfAbsent(j, k -> new HashSet<>()).add(i);
                 }
             }
         }
-        int res = 0;
         Set<Integer> seen = new HashSet<>();
-        for(Map.Entry<Integer, List<Integer>> g : graph.entrySet()) {
-            int node = g.getKey();
+        int res = 0;
+        for (Map.Entry<Integer, Set<Integer>> v : graph.entrySet()) {
+            int node = v.getKey();
             if (!seen.contains(node)) {
                 res++;
-                Queue<Integer> q = new LinkedList<>();
-                q.add(node);
                 seen.add(node);
-                while (!q.isEmpty()) {
-                    int v = q.poll();
-                    for (int nb : graph.get(v)) {
-                        if (!seen.contains(nb)) {
-                            q.add(nb);
-                            seen.add(nb);
-                        }
-                    }
+                for (int nb : v.getValue()) {
+                    seen.add(nb);
                 }
             }
         }
