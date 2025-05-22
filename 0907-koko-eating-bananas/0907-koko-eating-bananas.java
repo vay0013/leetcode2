44667206f53f1dd1f77c455609f084566fdbe11e
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-05-22 19:43:21 +1000

    Time: 12 ms (70.98%), Space: 45.4 MB (25.17%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,10 +1,9 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int left = 1;
-        int right = Integer.MAX_VALUE;
+        int left = 1, right = Integer.MAX_VALUE;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (canEating(piles, mid, h)) {
+            if (canEat(piles, h, mid)) {
                 right = mid - 1;
             } else {
                 left = mid + 1;
@@ -12,15 +11,13 @@ class Solution {
         }
         return left;
     }
-    private boolean canEating(int[] piles, int k, int h) {
-        int hours = 0;
+    private boolean canEat(int[] piles, int h, int speed) {
+        int spend = 0;
         for (int pile : piles) {
-            int div = pile / k;
-            hours += div;
-            if (pile % k != 0) {
-                hours++;
-            }
+            int div = pile / speed;
+            spend += div;
+            spend += pile % speed != 0 ? 1 : 0;
         }
-        return hours <= h;
+        return spend <= h;
     }
 }
