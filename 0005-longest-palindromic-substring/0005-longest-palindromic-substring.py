83a071f7395547a1d63485f24466f1e8ca56380b
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-05-30 20:06:04 +1000

    Time: 255 ms (84.74%), Space: 18.1 MB (28.44%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,23 +1,21 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def expand(i , j):
-            left = i
-            right = j
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                left -= 1
-                right += 1
-            return right - left - 1
+        def expand(p1, p2):
+            while p1 >= 0 and p2 < len(s) and s[p1] == s[p2]:
+                p1 -= 1
+                p2 += 1
+            return p2 - p1 - 1
 
         res = [0, 0]
 
-        for i in range(len(s)):
-            odd_length = expand(i, i)
-            if odd_length > res[1] - res[0] + 1:
-                dist = odd_length // 2
+        for i in range(len(s)-1):
+            odd_len = expand(i, i)
+            if odd_len > res[1] - res[0] + 1:
+                dist = odd_len // 2
                 res = [i - dist, i + dist]
-
-            even_length = expand(i, i + 1)
-            if even_length > res[1] - res[0] + 1:
-                dist = (even_length // 2) - 1
+    
+            even_len = expand(i, i+1)
+            if even_len > res[1] - res[0] + 1:
+                dist = even_len // 2 - 1
                 res = [i - dist, i + 1 + dist]
-        return s[res[0]:res[1]+1]
+        return s[res[0] : res[1] + 1]
