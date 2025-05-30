843a251bc40d8f24ddcaf8200fedca2aef90095c
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-05-30 19:27:40 +1000

    Time: 20 ms (40.21%), Space: 17.8 MB (52.96%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = set()
+        res = 0
+        start = 0
+        for end in range(len(s)):
+            while s[end] in seen:
+                seen.remove(s[start])
+                start += 1
+            seen.add(s[end])
+            res = max(res, end - start + 1)
+        return res
