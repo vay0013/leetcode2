017cbe78e3dac1260d3c0ef093f9cca80ad3fb79
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-05-30 19:33:05 +1000

    Time: 11 ms (16.1%), Space: 18.5 MB (68.85%) - LeetHub

--- 1028-interval-list-intersections/1028-interval-list-intersections.py
+++ 1028-interval-list-intersections/1028-interval-list-intersections.py
@@ -3,11 +3,12 @@ class Solution:
         res = []
         p1, p2 = 0, 0
         while p1 < len(f) and p2 < len(s):
-            start = max(f[p1][0], s[p2][0])
-            end = min(f[p1][1], s[p2][1])
-            if start <= end:
-                res.append([start, end])
-            if f[p1][1] < s[p2][1]:
+            cur = [0, 0]
+            cur[0] = max(f[p1][0], s[p2][0])
+            cur[1] = min(f[p1][1], s[p2][1])
+            if cur[0] <= cur[1]:
+                res.append(cur)
+            if cur[1] == f[p1][1]:
                 p1 += 1
             else:
                 p2 += 1
