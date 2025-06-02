c58fd40545779ddac6463f9545952e3ee9eeaea6
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-06-02 10:15:49 +1000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -6,7 +6,7 @@ class Solution {
             if (x - arr[mid] > arr[mid + k] - x) {
                 left = mid + 1;
             } else {
-                right = mid;
+                right = mid - 1;
             }
         }
         List<Integer> res = new ArrayList<>();
