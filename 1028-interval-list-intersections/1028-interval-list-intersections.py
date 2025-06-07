b8096787ef6d02d0d2acc7d1f0aa89dba57594ff
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-06-07 20:43:29 +1000

    Time: 6 ms (56.81%), Space: 18.6 MB (43.65%) - LeetHub

--- 1028-interval-list-intersections/1028-interval-list-intersections.py
+++ 1028-interval-list-intersections/1028-interval-list-intersections.py
@@ -1,14 +1,13 @@
 class Solution:
     def intervalIntersection(self, f: List[List[int]], s: List[List[int]]) -> List[List[int]]:
         res = []
-        p1, p2 = 0, 0
+        p1 = p2 = 0
         while p1 < len(f) and p2 < len(s):
-            cur = [0, 0]
-            cur[0] = max(f[p1][0], s[p2][0])
-            cur[1] = min(f[p1][1], s[p2][1])
-            if cur[0] <= cur[1]:
-                res.append(cur)
-            if cur[1] == f[p1][1]:
+            start = max(f[p1][0], s[p2][0])
+            end = min(f[p1][1], s[p2][1])
+            if start <= end:
+                res.append([start, end])
+            if end == f[p1][1]:
                 p1 += 1
             else:
                 p2 += 1
