cf5dcd930de877844a8ccb8da7bfde37eec86dfc
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-06-07 21:10:14 +1000

    Time: 23 ms (93.37%), Space: 62.4 MB (18.51%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-        int prefixMod = 0;
-        Map<Integer, Integer> seen = new HashMap<>();
-        seen.put(0, -1); // если первый эелемент nums[0] % k == 0 
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int sum = 0;
         for (int i = 0; i < nums.length; i++) {
-            prefixMod = (prefixMod + nums[i]) % k;
-            if (seen.containsKey(prefixMod)) {
-                if (i - seen.get(prefixMod) > 1) {
+            sum = (sum + nums[i]) % k;
+            if (map.containsKey(sum)) {
+                if (i - map.get(sum) > 1) {
                     return true;
-                }
+                } 
             } else {
-                seen.put(prefixMod, i);
+                map.put(sum, i);
             }
         }
         return false;
