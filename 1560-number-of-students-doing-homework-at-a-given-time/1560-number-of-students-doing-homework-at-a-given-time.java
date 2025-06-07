252654bac8329fc802888bde2602a83e02edb982
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-06-08 09:55:43 +1000

    Time: 0 ms (100%), Space: 41.9 MB (23.63%) - LeetHub

--- 1560-number-of-students-doing-homework-at-a-given-time/1560-number-of-students-doing-homework-at-a-given-time.java
+++ 1560-number-of-students-doing-homework-at-a-given-time/1560-number-of-students-doing-homework-at-a-given-time.java
@@ -1,8 +1,10 @@
 class Solution {
     public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
         int res = 0;
-        for (int i = 0; i < startTime.length; i++) {
-            if (startTime[i] <= queryTime && queryTime <= endTime[i]) {
+        for (int i = 0; i < endTime.length; i++) {
+            int start = startTime[i];
+            int end = endTime[i];
+            if (start <= queryTime && queryTime <= end) {
                 res++;
             }
         }
