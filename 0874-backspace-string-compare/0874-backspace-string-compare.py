af9d427c1f5c5413dc5093e256588e2566e99880
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-07-29 09:53:35 +1000

    Time: 0 ms (100%), Space: 17.9 MB (36.86%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.py
+++ 0874-backspace-string-compare/0874-backspace-string-compare.py
@@ -1,15 +1,14 @@
 class Solution:
-    def backspaceCompare(self, a: str, t: str) -> bool:
-        def build(s):
-            result = ""
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        def build(st):
+            res = ""
             skip = 0
-            for c in range(len(s)-1, -1, -1):
-                if s[c] == '#':
-                    skip+=1
+            for i in range(len(st)-1, -1, -1):
+                if st[i] == "#":
+                    skip += 1
                 elif skip > 0:
-                    skip-=1
+                    skip -= 1
                 else:
-                    result+=s[c]
-            return result
-        return build(a) == build(t)
-        
+                    res += st[i]
+            return res
+        return build(s) == build(t)
