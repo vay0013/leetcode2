c73a91ba54e1bf8d98b1e084abb5a0f8532a9e94
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-07-28 21:01:50 +1000

    Time: 249 ms (89.56%), Space: 17.8 MB (59.54%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -5,15 +5,16 @@ class Solution:
                 l -= 1
                 r += 1
             return r - l - 1
+            
         res = [0, 0]
         for i in range(len(s)-1):
             odd_len = expand(i, i)
             if res[1] - res[0] + 1 < odd_len:
                 dist = odd_len // 2
-                res = [i - dist, i + dist]
+                res = [i-dist, i+dist]
 
             even_len = expand(i, i+1)
             if res[1] - res[0] + 1 < even_len:
                 dist = even_len // 2 - 1
-                res = [i - dist, i + dist + 1]
-        return s[res[0] : res[1] + 1]
+                res = [i-dist, i+dist+1]
+        return s[res[0] : res[1]+1]
