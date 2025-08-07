a458cdc1203243fe7135702082eddc4d5c126909
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-08-07 13:46:17 +1000

    Time: 0 ms (100%), Space: 41.7 MB (22.36%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.java
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.java
@@ -2,8 +2,8 @@ class Solution {
     public int largestAltitude(int[] gain) {
         int res = 0;
         int sum = 0;
-        for (int num : gain) {
-            sum += num;
+        for (int i : gain) {
+            sum += i;
             res = Math.max(res, sum);
         }
         return res;
