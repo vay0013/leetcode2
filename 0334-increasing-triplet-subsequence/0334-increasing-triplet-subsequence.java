44f01380146a3f0e82c51fe6f8fd8bd1cada1b2e
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-08-07 12:52:21 +1000

    Time: 2 ms (96.44%), Space: 134.6 MB (7.36%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        int f = nums[0];
-        int s = Integer.MAX_VALUE;
-        for (int num : nums) {
-            if (num <= f) {
-                f = num;
-            } else if (num <= s) {
-                s = num;
+        int min1 = nums[0];
+        int min2 = Integer.MAX_VALUE;
+        for (int i : nums) {
+            if (min1 >= i) {
+                min1 = i;
+            } else if (min2 >= i) {
+                min2 = i;
             } else {
                 return true;
             }
