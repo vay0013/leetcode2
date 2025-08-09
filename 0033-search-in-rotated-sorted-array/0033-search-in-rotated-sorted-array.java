cdfa1d55c2971759a50da0bfc8641c4860fd9cf3
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-08-09 22:34:58 +1000

    Time: 0 ms (100%), Space: 42.3 MB (39.73%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -8,13 +8,13 @@ class Solution {
                 return mid;
             }
             if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && nums[mid] > target) {
-                    right = mid - 1;   
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
             } else {
-                if (nums[mid] < target && nums[right] >= target) {
+                if (nums[right] >= target && nums[mid] < target) {
                     left = mid + 1;
                 } else {
                     right = mid - 1;
