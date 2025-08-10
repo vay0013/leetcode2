8a289ed63604ed65d4425d320e47a3c450224805
vay0013 <88012131+vay0013@users.noreply.github.com>
2025-08-10 21:48:45 +1000

    Time: 2 ms (26.38%), Space: 45.6 MB (30.75%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,9 +1,5 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int res = nums.length;
-        for (int i = 0; i < nums.length; i++) {
-            res += i - nums[i];
-        }
-        return res;
+        return (nums.length * (nums.length+1)) / 2 - Arrays.stream(nums).sum();
     }
 }
